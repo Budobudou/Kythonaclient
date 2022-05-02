c01c3371ecd6c7a73c1e6d26057921afad721002
Budobudou <63654313+Budobudou@users.noreply.github.com>
2022-05-02 22:22:27 +0900

    超大型アップデート！

--- kythona.py
+++ kythona.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # Kythonaの読み方は「かいそな」です。
 # Kythona Loader#
-import requests, json, getpass, io ,sys , time,re
-savedir = "kythonaSE.txt"
+import requests,json,getpass,io,sys,time,re,os,readline
+homedir = os.path.expanduser("~")
+os.chdir(homedir)
+savedir = f"{homedir}//.kythonaSE.txt"
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-print("KythonaClient Version 1.2")
+print("\033[36m"+"* KythonaClient"+"\033[0m"+" Version 2.0")
 apiurl = "https://kana.renorari.net/api/api.json"
 try:
 	file = open(savedir, "r")
@@ -19,13 +21,12 @@ try:
 
 except FileNotFoundError:
 	print("[Login] KanaAPIのIDとパスワードを入力してください \n ※ パスワードは表示されません。")
-	
-
-	logid = input("ID>")
+	logid = input("ID> ")
 	time.sleep(0.3)
-	logpass = getpass.getpass("Pass>")
+	logpass = getpass.getpass("Pass> ")
 	kdict = "https://raw.githubusercontent.com/renorari/Kana-dictionary/main/discord.kana"
 # Login And Test #
+	print("ログインテスト中...")
 	logpost = f"message=login_test&id={logid}&password={logpass}&customize_url=none"
 	login1 = requests.post(apiurl, data=logpost.encode("utf-8"))
 	login2 = login1.text
@@ -39,20 +40,26 @@ except FileNotFoundError:
 		writedata.write(f"{logpass}\n")
 		writedata.write(f"{kdict}\n")
 		writedata.close()
-print(">>Kythonaへようこそ。\nコマンド一覧は/helpと入力してください。")
+		print("ログインに成功しました。Kythona(かいそな)へようこそ！")
+print("コマンド一覧は/helpと入力してください。")
 
 # Bot Talk #
 while True:
-	rawmsg = input("Kythona>")
+	rawmsg = input("Kythona> ")
 	if rawmsg == "/help":
-		print("・/help 今実行したコマンドだよ。\n・/exit KythonaClientを終了するよ。")
+		print("・/help 今実行したコマンドだよ。\n・/exit KythonaClientを終了するよ。\n・/json 直前のメッセージの詳細を表示するよ。")
 	elif rawmsg == "/exit":
 		sys.exit()
+		print("何もしていないのに終了できなかった、ごめん^^")
 	elif rawmsg.startswith("/cdict "):
 		kdict = rawmsg[7:]
 		print(f"辞書を{kdict}に変更しました。")
 	elif rawmsg == "/json":
-		print(data4)
+		try:
+			print(data3)
+		except NameError:
+			print("Kythonaでエラーが発生しました")
+			print("Jsonデータがありません")
 	else:
 		msg = str(rawmsg)
 		postd = f"message={msg}&id={logid}&password={logpass}&customize_url={kdict}"
@@ -63,6 +70,14 @@ while True:
 			print(data3)
 			print(f"ログイン情報：\nID:{logid}\nPass:{logpass}\nDictURL:{kdict}")
 			sys.exit()
-		data4 = json.loads(data3)
-		dataf = data4["reply"]
-		print(dataf)
+		else:
+			# Extension
+			data4 = json.loads(data3)
+			if "timer" in data4["extension"].keys():
+				print(data4["extension"]["timer"]["start_message"])
+				sys.stdout.flush()
+				time.sleep(data4["extension"]["timer"]["time"])
+				print(data4["extension"]["timer"]["end_message"])
+			else:
+				dataf = data4["reply"]
+				print(dataf)
